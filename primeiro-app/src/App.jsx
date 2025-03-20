25bc117a7adb76cf7c706c50177b1916e2c166c1
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-03-20 19:28:30 -0300

    learn: aprendendo react

--- primeiro-app/src/App.jsx
+++ primeiro-app/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div
+      style={{
+        margin: "auto",
+        width: "768px",
+        display: "flex",
+        flexDirection: "column",
+        background: "#e9e9e9",
+        padding: "12px",
+        borderRadius: "12px",
+        fontFamily: "font-family: Arial",
+      }}
+    >
+      <label htmlFor="nome">Nome:</label>
+      <input
+        type="text"
+        placeholder="Victor de brito Laranjeira"
+        style={{
+          outline: "none",
+          padding: "8px",
+          border: "none",
+          margin: "5px 0",
+          borderRadius: "8px",
+        }}
+      />
+      <button
+        style={{
+          background: "#8a2be2",
+          color: "#fff",
+          fontWeight: "bolder",
+          cursor: "pointer",
+          padding: "8px",
+          border: "none",
+          borderRadius: "8px",
+        }}
+      >
+        Enviar
+      </button>
+    </div>
+  );
+};
+export default App;
